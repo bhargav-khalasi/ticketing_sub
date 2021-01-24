6b04ab4baa23609eb8027e29b1298374743fa75e
develper <bkhalasi404@gmail.com>
2021-01-24 15:03:08 +0530

    add auth service

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,81 @@
+import request from 'supertest';
+
+import { app } from '../../app';
+
+it('returns a 201 on successful signup', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+});
+
+it('returns a 400 on invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testasfca.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('returns a 400 on invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'pas',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: '',
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: '',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('Bad Request on duplicate email', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('Sets a cookie after successfull signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
